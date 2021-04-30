a35741a45e751f1973fa4c77e55619a13f8f0e5d
Lyudmila Balan <milabala.ua@gmail.com>
2021-04-30 06:55:24 +0300

    Changing second.py

---
+++ Day1/composition.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self):
+        page1=Page('page 1')
+        page2=Page('page 2')
+        self.pages=[page1,page2]
+
+class Page:
+    def __init__(self,content):
+        self.content=content
+
+our_book=Book()
+for page in 
